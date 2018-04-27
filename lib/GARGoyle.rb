a9182c5502d62fd8cefbc48afe01e43e861dfe0a
Greg Rebuck <greg@tkraftsmen.com>
2018-04-26 21:21:08 -0500

    second, first commit

---
+++ lib/GARGoyle.rb
@@ -0,0 +1,5 @@
+require "GARGoyle/version"
+
+module GARGoyle
+  # Your code goes here...
+end
---
+++ spec/GARGoyle_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GARGoyle do
+  it "has a version number" do
+    expect(GARGoyle::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
