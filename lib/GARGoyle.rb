1a3d9215e42473bdebbea71eea6e29c363f11b81
Greg Rebuck <greg@tkraftsmen.com>
2018-05-07 23:11:27 -0500

    Final solution

--- lib/GARGoyle.rb
+++ lib/GARGoyle.rb
@@ -1,12 +1,35 @@
-require "GARGoyle/version"
+require 'GARGoyle/version'
 
 module GARGoyle
+  # Extends standard Hash class to include alphabetical topology sorting
+  class SortedHash < Hash
+    include TSort
+
+    def tsort_each_node(&block)
+      keys.sort.each(&block)
+    end
+
+    def tsort_each_child(node, &block)
+      fetch(node, []).each(&block)
+    end
+  end
+
   # Takes in list of jobs and returns a list ordered by dependency (if any)
   # and then natural order
   class JobSequencer
+
     def process(jobs)
-      sequence = []
-      sequence + jobs.keys.collect(&:to_s)
+      sortable_jobs = SortedHash.new { |h, k| h[k] = [] }
+      jobs.each do |k, v|
+        k, v = k.to_s, v.to_s
+        raise "Job and dependency can't be the same" if k == v
+        sortable_jobs[k] << v
+      end
+      begin
+        sortable_jobs.tsort.reject(&:empty?)
+      rescue TSort::Cyclic
+        raise 'There was a circular job dependency'
+      end
     end
   end
 end
--- spec/GARGoyle_spec.rb
+++ spec/GARGoyle_spec.rb
@@ -1,16 +1,43 @@
+require 'GARGoyle.rb'
+
 RSpec.describe GARGoyle do
-  it "is an empty sequence" do
+
+  it 'is an empty sequence' do
     sequencer = GARGoyle::JobSequencer.new
     expect(sequencer.process({})).to eq([])
   end
 
-  it "is has a sequence of one job" do
+  it 'has a sequence of one job' do
     sequencer = GARGoyle::JobSequencer.new
     expect(sequencer.process(a: '')).to eq(['a'])
   end
 
-  it "is has a sequence of three jobs" do
+  it 'has a sequence of three jobs' do
     sequencer = GARGoyle::JobSequencer.new
     expect(sequencer.process(a: '', b: '', c: '')).to eq(%w[a b c])
+  end
+
+  it 'has a sequence of three jobs with a dependency' do
+    sequencer = GARGoyle::JobSequencer.new
+    expect(sequencer.process(a: '', b: 'c', c: '')).to eq(%w[a c b])
+  end
+
+  it 'has a sequence of six jobs with chained dependencies' do
+    sequencer = GARGoyle::JobSequencer.new
+    expect(sequencer.process(a: '', b: 'c', c: 'f', d: 'a', e: 'b', f: '')).to eq(%w[a f c b d e])
+  end
+
+  it 'has a sequence of three jobs and a circular dependency' do
+    sequencer = GARGoyle::JobSequencer.new
+    expect do
+      sequencer.process(a: '', b: '', c: 'c')
+    end.to raise_error(StandardError, "Job and dependency can't be the same")
+  end
+
+  it 'has a sequence of six jobs with a circular dependency' do
+    sequencer = GARGoyle::JobSequencer.new
+    expect do
+      sequencer.process(a: '', b: 'c', c: 'f', d: 'a', e: '', f: 'b')
+    end.to raise_error(StandardError, 'There was a circular job dependency')
   end
 end
